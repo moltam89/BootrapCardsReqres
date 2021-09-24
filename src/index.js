6739b61f8a08cf88c54bf63acca2847357166d21
Tamas Molnar <tamas.molnar@liferay.com>
2021-09-24 13:38:48 +0200

    Roli fix

--- src/index.js
+++ src/index.js
@@ -1,18 +1,31 @@
+import 'regenerator-runtime/runtime'
 import Card from "./card";
 import Form from "./form";
 import { processTemplate } from "./util.js";
 
 import "./styles.css";
 
-let app = document.getElementById("app");
-let id = 10;
+let Users = {}
 
+let app = document.getElementById("app");
+
+let minId = 0;
+const MAX_ID = 10000;
+const modal = new bootstrap.Modal(document.getElementById("profileModal"));
+const modalBody = document.querySelector(".modal-body");
+
+const saveButton = document.querySelector("#saveButton");
+saveButton.addEventListener(
+	"click",
+	saveButtonClick
+);
+	
 showUserCards();
 createNewProfileButton();
 
 function getEmptyData() {
   return {
-    id: id++,
+    id: 8,
     email: "",
     first_name: "",
     last_name: "",
@@ -20,11 +33,21 @@ function getEmptyData() {
   };
 }
 
+function setMinId(newId){
+	if(newId > minId) {
+		minId = newId;
+	}
+}
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+}
+
 function createElementFromHTML(htmlString) {
   var div = document.createElement("div");
   div.innerHTML = htmlString.trim();
-
-  // Change this to div.childNodes to support multiple top-level nodes
   return div.firstChild;
 }
 
@@ -37,34 +60,34 @@ function createNewProfileButton() {
   button.innerHTML = "Add new profile";
   button.classList = "btn btn-primary";
 
-  button.addEventListener(
-    "click",
-    () => {
-      openProfileModal(getEmptyData());
-    },
-    { once: true }
-  );
+  button.addEventListener("click", () => {
+    openProfileModal(getEmptyData());
+  });
 
   divWrapper.append(button);
   app.appendChild(divWrapper);
 }
 
-async function deleteUser(id, confirmModal) {
-  let response = await axios.delete("https://reqres.in/api/users/" + id);
+function deleteUser(id, confirmModal) {
+  axios.delete("https://reqres.in/api/users/" + id)
+    .then(function (response) {
+	  console.log(response);
+	  var card = document.getElementById("card" + id);
 
-  console.log(response);
-  var card = document.getElementById("card" + id);
+	  if (card) {
+		card.remove();
+	  }
 
-  if (card) {
-    card.remove();
-  }
-
-  if (confirmModal) {
-    confirmModal.hide();
-  }
+	  if (confirmModal) {
+		confirmModal.hide();
+	  }
+  })
 }
 
-function openDeleteModal(data) {
+function openDeleteModal(e) {
+  let id = e.currentTarget.dataset.id;
+  let data = Users[id];
+  
   let confirmModal = new bootstrap.Modal(
     document.getElementById("confirmModal")
   );
@@ -82,66 +105,69 @@ function openDeleteModal(data) {
   );
 }
 
-function addSaveButtonEventListener(data, modal) {
-  console.log("addSaveButtonEventListener");
-  let first_name = document.querySelector("#formGroupExampleInput1").value;
-  let last_name = document.querySelector("#formGroupExampleInput2").value;
-  let avatar = document.querySelector("#formGroupExampleInput3").value;
-  let email = document.querySelector("#exampleInputEmail1").value;
+function saveButtonClick (e) {
+	let id, user = {};
+	
+	if (e.currentTarget && e.currentTarget.dataset && e.currentTarget.dataset.id) {
+		id = e.currentTarget.dataset.id;
+		user = Users[id];
+	}
+	
+	  let first_name = document.querySelector("#formGroupExampleInput1").value;
+	  let last_name = document.querySelector("#formGroupExampleInput2").value;
+	  let avatar = document.querySelector("#formGroupExampleInput3").value;
+	  let email = document.querySelector("#exampleInputEmail1").value;
 
-  if (first_name != "") {
-    data.first_name = first_name;
-  }
+	  if (first_name != "") {
+		user.first_name = first_name;
+	  }
 
-  if (last_name != "") {
-    data.last_name = last_name;
-  }
+	  if (last_name != "") {
+		user.last_name = last_name;
+	  }
 
-  if (email != "") {
-    data.email = email;
-  }
+	  if (email != "") {
+		user.email = email;
+	  }
 
-  if (avatar != "") {
-    data.avatar = avatar;
-  }
+	  if (avatar != "") {
+		user.avatar = avatar;
+	  }
 
-  data.name = data.first_name + ` ` + data.last_name;
+	  user.name = user.first_name + ' ' + user.last_name;
 
-  let oldCard = document.querySelector("#card" + data.id);
-  if (!oldCard) {
-    createUser(data);
-    modal.hide();
-    return;
-  }
-  debugger;
-  oldCard.querySelector("h5").innerHTML = data.name;
-  if (avatar != "") {
-    oldCard.querySelector("img").src = data.avatar;
-  }
+	  let oldCard = document.querySelector("#card" + user.id);
+	  if (!oldCard) {
+		createUser(user);
+		modal.hide();
+		return;
+	  }
 
-  if (first_name != "" || last_name != "") {
-    updateUser(data.id, data.name);
-  }
+	  oldCard.querySelector("h5").innerHTML = user.name;
+	  if (avatar != "") {
+		oldCard.querySelector("img").src = user.avatar;
+	  }
 
-  modal.hide();
+	  if (first_name != "" || last_name != "") {
+		updateUser(user.id, user.name);
+	  }
+
+	  modal.hide();
 }
 
-function openProfileModal(data) {
-  const modal = new bootstrap.Modal(document.getElementById("profileModal"));
-  const modalBody = document.querySelector(".modal-body");
-  modalBody.innerHTML = processTemplate(new Form().getHTML(), data);
-  modal.show();
 
-  let saveButton = document.querySelector("#saveButton");
-  saveButton.removeEventListener("click", addSaveButtonEventListener);
-
-  saveButton.addEventListener(
-    "click",
-    addSaveButtonEventListener.bind(null, data, modal),
-    {
-      once: true
-    }
-  );
+ 
+function openProfileModal(e) {
+	let id, user = {};
+	
+	if (e.currentTarget && e.currentTarget.dataset && e.currentTarget.dataset.id) {
+		id = e.currentTarget.dataset.id;
+		user = Users[id];
+		saveButton.dataset.id = id;
+	}
+	
+	modalBody.innerHTML = processTemplate(new Form().getHTML(), user);
+    modal.show();
 }
 
 function showUserCard(data) {
@@ -155,48 +181,47 @@ function showUserCard(data) {
   );
 
   const profileButton = document.querySelector("#profileButton" + data.id);
-
-  profileButton.addEventListener(
-    "click",
-    () => {
-      openProfileModal(data);
-    },
-    { once: true }
-  );
+  profileButton.addEventListener("click", openProfileModal);
 
   const deleteButton = document.querySelector("#deleteButton" + data.id);
-
-  deleteButton.addEventListener(
-    "click",
-    () => {
-      openDeleteModal(data);
-    },
-    { once: true }
-  );
+  deleteButton.addEventListener("click", openDeleteModal);
 }
 
-async function showUserCards() {
-  let response = await axios.get("https://reqres.in/api/users");
-  for (let x in response.data.data) {
-    showUserCard(response.data.data[x]);
-  }
+function showUserCards() {
+  axios.get("https://reqres.in/api/users")
+  .then(function (response) {
+	  for (let x in response.data.data) {
+		let user = response.data.data[x];
+		Users[user.id] = user;  
+		
+		setMinId(user.id);
+		showUserCard(user);
+	  }
+  })
 }
 
-async function updateUser(id, name) {
-  console.log("updateUser");
-  let response = await axios.put("https://reqres.in/api/users/" + id, {
+function updateUser(id, name) {
+   axios.put("https://reqres.in/api/users/" + id, {
     name: "name"
-  });
-
-  console.log(response);
+  })
+  .then(function (response) {
+    console.log(response);
+  })
 }
 
-async function createUser(data) {
-  let response = await axios.put("https://reqres.in/api/users/", {
-    name: data.first_name,
-    job: "leader"
-  });
-
-  console.log(response);
-  showUserCard(data);
+function createUser(user) {
+  axios.put("https://reqres.in/api/users/", 
+    user)
+  .then(function (response) {
+	  console.log(response);
+	  let id = response.data.id;
+	 
+	  if (id == null) {
+		  id = getRandomInt(minId, MAX_ID);
+	  } 
+	  
+	  user.id = id;
+	  Users[user.id] = user;
+	  showUserCard(user);
+  })
 }
